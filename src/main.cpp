b4bf7d98d00cbce9466f2b8c89b5af53c095f5dd
Michael <mvasilyev04@gmail.com>
2026-07-04 02:20:32 +0300

    указал размер окна и создал Главное окно

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
-#include <QApplication>
+#include "ui/MainWindow.hpp"
+#include <QAction>
 
-
-int main() {
-    return 0;
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    MainWindow window{};
+    window.setWindowTitle("FastGet");
+    window.resize(1920, 1080);
+    window.show();
+    return app.exec();
 }
