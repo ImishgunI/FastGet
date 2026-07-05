4f3620ff99e3966509297bda7e8f4d4f2f231620
Michael <mvasilyev04@gmail.com>
2026-07-05 23:03:20 +0300

    Почти доделал окно для add

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -1,9 +1,9 @@
 #include "MainWindow.hpp"
 #include <QMenuBar>
 #include <QToolBar>
-#include <qaction.h>
-#include <qnamespace.h>
-#include <qwidget.h>
+#include <QLabel>
+#include <QLineEdit>
+#include <QBoxLayout>
 //    +---------------------------------------------------------------+
 //    | File                    Settings                              |
 //    +---------------------------------------------------------------+
@@ -49,7 +49,19 @@ void MainWindow::createToolBar() {
 void MainWindow::addAction() {
     QWidget* addWindow = new QWidget();
     addWindow->setWindowTitle("Add new download");
-    addWindow->resize(800, 600);
+    addWindow->resize(300, 200);
+    QLabel* url = new QLabel("&URL:");
+    QLineEdit* url_line = new QLineEdit;
+    url->setBuddy(url_line);
+    QLabel* save_path = new QLabel("&Save:");
+    QLineEdit* save_path_line = new QLineEdit;
+    save_path->setBuddy(save_path_line);
+    QVBoxLayout* bl = new QVBoxLayout;
+    bl->addWidget(url);
+    bl->addWidget(url_line);
+    bl->addWidget(save_path);
+    bl->addWidget(save_path_line);
+    addWindow->setLayout(bl);
     addWindow->show();
 }
 
