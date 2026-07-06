8cb3118eaf926a00d4539fdf0130eed963a05fda
Michael <mvasilyev04@gmail.com>
2026-07-06 06:22:24 +0300

    Указал для всех классов родителя, добавил кнопки cancel и accept, повесил на них connect, и прописал эти кнопки в горизонтальный box

--- src/ui/AddDownload.cpp
+++ src/ui/AddDownload.cpp
@@ -2,21 +2,28 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QBoxLayout>
+#include <QPushButton>
 
 AddDownloadDialog::AddDownloadDialog(QWidget* parent) : QDialog(parent) {
-    QWidget* addWindow = new QWidget();
-    addWindow->setWindowTitle("Add new download");
-    QLabel* url = new QLabel("&URL:");
-    QLineEdit* url_line = new QLineEdit;
+    setWindowTitle("Add New Download");
+    resize(250, 150);
+    QLabel* url = new QLabel("&URL:", this);
+    QLineEdit* url_line = new QLineEdit(this);
     url->setBuddy(url_line);
-    QLabel* save_path = new QLabel("&Save:");
-    QLineEdit* save_path_line = new QLineEdit;
+    QLabel* save_path = new QLabel("&Save:", this);
+    QLineEdit* save_path_line = new QLineEdit(this);
     save_path->setBuddy(save_path_line);
-    QVBoxLayout* bl = new QVBoxLayout;
+    QVBoxLayout* bl = new QVBoxLayout(this);
     bl->addWidget(url);
     bl->addWidget(url_line);
     bl->addWidget(save_path);
     bl->addWidget(save_path_line);
-    addWindow->setLayout(bl);
-    addWindow->show();
+    QHBoxLayout* buttons_box = new QHBoxLayout(this);
+    QPushButton* cancel_button = new QPushButton("&Cancel", this);
+    buttons_box->addWidget(cancel_button);
+    QPushButton* accept_button = new QPushButton("&Accept", this);
+    buttons_box->addWidget(accept_button);
+    bl->addLayout(buttons_box);
+    connect(cancel_button, &QPushButton::clicked, this, &QDialog::accept);
+    connect(accept_button, &QPushButton::clicked, this, &QDialog::reject);
 }
