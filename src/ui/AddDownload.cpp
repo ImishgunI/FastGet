6d51d3ca457dd127a74f0f7dedf8e8441d0ef661
Michael <mvasilyev04@gmail.com>
2026-07-06 05:52:49 +0300

    Добавил код из addAction в конструктор

--- src/ui/AddDownload.cpp
+++ src/ui/AddDownload.cpp
@@ -1,3 +1,22 @@
 #include "AddDownload.hpp"
+#include <QLabel>
+#include <QLineEdit>
+#include <QBoxLayout>
 
-
+AddDownloadDialog::AddDownloadDialog(QWidget* parent) : QDialog(parent) {
+    QWidget* addWindow = new QWidget();
+    addWindow->setWindowTitle("Add new download");
+    QLabel* url = new QLabel("&URL:");
+    QLineEdit* url_line = new QLineEdit;
+    url->setBuddy(url_line);
+    QLabel* save_path = new QLabel("&Save:");
+    QLineEdit* save_path_line = new QLineEdit;
+    save_path->setBuddy(save_path_line);
+    QVBoxLayout* bl = new QVBoxLayout;
+    bl->addWidget(url);
+    bl->addWidget(url_line);
+    bl->addWidget(save_path);
+    bl->addWidget(save_path_line);
+    addWindow->setLayout(bl);
+    addWindow->show();
+}
