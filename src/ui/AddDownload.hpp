0c8096fae2120450c1d74ac096bf8f0759ad9596
Michael <mvasilyev04@gmail.com>
2026-07-07 19:12:50 +0300

    Доделал UI для модального окна

--- src/ui/AddDownload.hpp
+++ src/ui/AddDownload.hpp
@@ -1,9 +1,22 @@
 #pragma once
 
 #include <QDialog>
+#include <QString>
+#include <QLineEdit>
 
 class AddDownloadDialog : public QDialog {
 public:
     explicit AddDownloadDialog(QWidget *parent = nullptr);
     ~AddDownloadDialog() override = default;
+
+    QString url() const;
+    QString savePath() const;
+
+private:
+    void browseSaveDir();
+    QString fileNameFromUrl();
+
+private:
+    QLineEdit* url_line;
+    QLineEdit* save_path_line;
 };
