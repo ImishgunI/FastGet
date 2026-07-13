f1860c5e1c6022e89081c05bf9e586d773e02e79
Michael <mvasilyev04@gmail.com>
2026-07-13 19:31:53 +0300

    Добавил сеттеров и геттеров

--- src/core/DownloadTask.hpp
+++ src/core/DownloadTask.hpp
@@ -19,6 +19,18 @@ public:
     DownloadTask(const QUrl& url, const QString& savePath);
     ~DownloadTask() = default;
 
+    void setStatus(DownloadStatus status);
+    void setDownloadedBytes(quint64 dbytes);
+    void setTotalBytes(quint64 tbytes);
+    void setSpeed(double speed);
+
+    QUrl getUrl() const;
+    QString getSavePath() const;
+    DownloadStatus getStatus() const;
+    quint64 getDownloadedBytes() const;
+    quint64 getTotalBytes() const;
+    double getSpeed() const;
+
 private:
     QUrl url;
     QString savePath;
