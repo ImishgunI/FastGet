5524197f9a0d0c41d143f2d1ec7ae92c83edd409
Michael <mvasilyev04@gmail.com>
2026-07-23 03:01:43 +0300

    Реализовал DownloadManager

--- src/core/DownloadManager.cpp
+++ src/core/DownloadManager.cpp
@@ -1,5 +1,32 @@
 #include "DownloadManager.hpp"
+#include <memory>
 
-void DownloadManager::addTask(const QUrl& url, const QString& savePath) {
+DownloadManager::DownloadManager(QObject* parent) : QObject(parent) {}
+
+DownloadTask& DownloadManager::addTask(const QUrl& url, const QString& savePath) {
     tasks.emplace_back(std::make_unique<DownloadTask>(url, savePath));
+    auto worker = std::make_unique<DownloadWorker>();
+    connect(worker.get(), &DownloadWorker::taskProgressChanged, this, &DownloadManager::onTaskProgressChanged);
+    activeWorkers.emplace_back(std::move(worker));
+    return *tasks.back();
+}
+
+void DownloadManager::startTask(int index) {
+    activeWorkers[index]->startDownload(*tasks[index]);
+}
+
+void DownloadManager::removeTask(int index) {
+    if (index >= activeWorkers.size() || index < 0) {
+        return;
+    }
+    activeWorkers[index]->cancelDownload();
+}
+
+void DownloadManager::eraseVector(int index) {
+    tasks.erase(tasks.begin() + index);
+    activeWorkers.erase(activeWorkers.begin() + index);
+}
+
+void DownloadManager::onTaskProgressChanged(DownloadTask* task) {
+    emit taskUpdated(*task);
 }
--- src/core/DownloadManager.hpp
+++ src/core/DownloadManager.hpp
@@ -1,16 +1,29 @@
 #pragma once
 
 #include "DownloadTask.hpp"
+#include "../network/DownloadWorker.hpp"
 
 #include <memory>
+#include <qobject.h>
 #include <vector>
 #include <QUrl>
 #include <QString>
 
-class DownloadManager {
+class DownloadManager : public QObject {
+Q_OBJECT
 public:
-    void addTask(const QUrl& url, const QString& savePath);
+    explicit DownloadManager(QObject* parent = nullptr);
+    ~DownloadManager() override = default;
+
+    DownloadTask& addTask(const QUrl& url, const QString& savePath);
+    void startTask(int index);
+    void removeTask(int index);
+    void eraseVector(int index);
+    void onTaskProgressChanged(DownloadTask* task);
+signals:
+    void taskUpdated(DownloadTask& task);
 
 private:
     std::vector<std::unique_ptr<DownloadTask>> tasks;
+    std::vector<std::unique_ptr<DownloadWorker>> activeWorkers;
 };
